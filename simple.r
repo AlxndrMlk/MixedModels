c9a3e1d4b89576dd812955763bf41fd7b2adf894
Aleksander Molak <aleksander.molak@gmail.com>
2019-03-07 21:25:58 +0100

    Update simple.r

--- simple.r
+++ simple.r
@@ -1,6 +1,6 @@
 library(lme4)
 
-# Fit a model with one fixed (`x`) and one random (`group`) effect
+# Fit a model with one fixed (`x`) and one random (`group`) effect (intercept only)
 # Note the specific syntax (`(1|<your_var>)`) for random effect
 outLmer <- lmer(response ~ x + (1|group), data = multIntDemo)
 
